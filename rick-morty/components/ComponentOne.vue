4ef9be439e85938c3bbf2fa89f7c1adfb3ec0ce2
Maarten van Ee <maartenvanee@gmail.com>
2019-10-17 15:52:03 +0200

    sass and first component

---
+++ rick-morty/components/ComponentOne.vue
@@ -0,0 +1,6 @@
+<template>
+  <h1>Component 1</h1>
+</template>
+
+<style>
+</style>
---
+++ rick-morty/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="page-index">
+    <ComponentOne />
+    <ComponentOne />
+    <ComponentOne />
+  </div>
+</template>
+
+<script>
+import ComponentOne from "~/components/ComponentOne.vue";
+
+export default {
+  components: {
+    ComponentOne
+  }
+};
+</script>
+
+<style>
+</style>
